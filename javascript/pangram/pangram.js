3f246e71cdaa3de26252dc2f89a165e9df4ab46a
dk <denis.kolokolov@icloud.com>
2022-11-22 10:23:07 +0100

    pangram

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -2,14 +2,30 @@
 // This is only a SKELETON file for the 'Pangram' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
+
+// Creating an array of alphabet
 const alphabet = [...Array(26)].map((_, i) => String.fromCharCode(i + 97))
+// Creating an object of alphabet where every letter is a key, and initail value equals 0
+const alphabetObject = Object.fromEntries(alphabet.map(char => ([char, 0])))
+// console.log(alphabetObject)
+
 
 export const isPangram = (sentence) => {
-  console.log(alphabet)
 
-  if (sentence === "") {
-    return false
-  }
+  // Converting sentence into character array
+  const charArr = sentence.toLowerCase().split('')
 
+  // Counting how ofter every char occers in the character array
+  const result = charArr.reduce((charArrFreq, char) => {
+    if (charArrFreq[char] == null) {
+      charArrFreq[char] = 1;
+    } else {
+      charArrFreq[char] += 1;
+    }
+    return charArrFreq;
+  }, {});
 
+  // Checking if every letter from the alphabet array exists in result
+  const isInAlphabet = (currentChar) => result.hasOwnProperty(currentChar);
+  return alphabet.every(isInAlphabet);
 };
