6db3c616eb6857d08494b09a35353fc65c190ec4
Denis Kolokolov <dennis.kolokolov@gmail.com>
2022-12-16 08:53:28 +0100

    matching-brackets all tests completed

--- javascript/matching-brackets/matching-brackets.js
+++ javascript/matching-brackets/matching-brackets.js
@@ -3,25 +3,26 @@
 // convenience to get you started writing code faster.
 //
 
-export const isPaired = (input) => {
+export const isPaired = (str) => {
+  const input = str.replaceAll(' ', '') // + Regex expression to filter only brackets
   const stack = []
   const brackets = { '[': ']', '{': '}', '(': ')' }
 
   for (let i = 0; i < input.length; i++) {
-    // If true than it's an opening bracket - push it to stack
-    if (Object.keys(brackets).includes(input[i])) {
-      stack.push(input[i])
-      console.log(stack)
-    } else {
-      // console.log("else", brackets[stack[0]] === input[i])
-
-      brackets[stack[0]] === input[i] ? stack.shift() : stack.push(input[i])
-        
-      
-    }
+    // Only if input[i] is a key or a value from brackets object do the logic:
+    if (Object.keys(brackets).includes(input[i]) || Object.values(brackets).includes(input[i])) {
+      // If true than it's an opening bracket - push it to stack
+      if (Object.keys(brackets).includes(input[i])) {
+        stack.push(input[i])
+      // If it is a closing bracket whose opening equivalent is at the top of the stack remove it from the stack
+      } else if (brackets[stack[stack.length - 1]] === input[i]) {
+        stack.pop()
+      // Add anything else (e.g. first char is a closing bracket) to the stack
+      } else {
+        stack.push(input[i])
+      }
+    }   
   }
-
-  console.log(stack)
   return stack.length === 0 ? true : false
 };
 
