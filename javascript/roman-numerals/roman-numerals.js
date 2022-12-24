81818534678f40dbbccbeab2cda4d3ba2b3d17ce
Denis Kolokolov <dennis.kolokolov@gmail.com>
2022-12-24 17:41:30 +0100

    roman-numerals final version

--- javascript/roman-numerals/roman-numerals.js
+++ javascript/roman-numerals/roman-numerals.js
@@ -4,29 +4,28 @@
 
 const ROMAN = {
   M: 1000,
-  CM: 900,
-  D: 500,
-  CD: 400,
-  C: 100,
-  XC: 90,
-  L: 50,
-  XL: 40,
-  X: 10,
-  IX: 9,
-  V: 5,
-  IV: 4,
-  I: 1
-}
+ CM:  900,
+  D:  500,
+ CD:  400,
+  C:  100,
+ XC:   90,
+  L:   50,
+ XL:   40,
+  X:   10,
+ IX:    9,
+  V:    5,
+ IV:    4,
+  I:    1,
+};
 
 export const toRoman = (num) => {
   let romanString = ""
 
-  Object.entries(ROMAN).forEach(item => {
-    while(num >= item[1]) {
-      num -= item[1]
-      romanString += item[0]
+  for (let letters in ROMAN) {
+    while (num >= ROMAN[letters]) {
+      num -= ROMAN[letters];
+      romanString += letters;
     }
-  })
-
+  }
   return romanString
 };
