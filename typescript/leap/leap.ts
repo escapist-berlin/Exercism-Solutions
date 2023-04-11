ec9285b0b05fd4762ea44c8fd91ba1ac15a75ba1
Denis Kolokolov <dennis.kolokolov@gmail.com>
2023-04-11 08:35:41 +0200

    [OPT] TypeScript leap

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,16 +1,10 @@
-type LeapYearResult = boolean
-
-export const isLeap = (year: number): LeapYearResult => {
-  const isDivisibleBy4: boolean = year % 4 === 0
-  const isDivisibleBy100: boolean = year % 100 === 0
-  const isDivisibleBy400: boolean = year % 400 === 0
-
+export const isLeap = (year: number): boolean => {
   switch (true) {
-    case isDivisibleBy400:
+    case year % 400 === 0:
       return true
-    case isDivisibleBy100:
+    case year % 100 === 0:
       return false
-    case isDivisibleBy4:
+    case year % 4 === 0:
       return true
     default:
       return false
