99a464367786bb5cc475cc1c40da5894a1cf503b
Denis Kolokolov <dennis.kolokolov@gmail.com>
2023-04-12 10:12:44 +0200

    [ADD] TypeScript resistor-color

---
+++ typescript/resistor-color/resistor-color.ts
@@ -0,0 +1,20 @@
+interface ColorCode {
+  [color: string]: number
+}
+
+const colorCodes: ColorCode = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9
+}
+
+export const colorCode = (color: string): number => colorCodes[color]
+
+export const COLORS = Object.keys(colorCodes)
