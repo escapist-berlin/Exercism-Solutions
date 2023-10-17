c70885bb61ed60c742184169d20f6615db108064
Denis Kolokolov <dennis.kolokolov@gmail.com>
2023-10-17 17:05:07 +0200

    transpose: empty string

---
+++ javascript/transpose/transpose.js
@@ -0,0 +1,10 @@
+//
+// This is only a SKELETON file for the 'Transpose' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const transpose = (input) => {
+  if (input.length === 0) {
+    return input
+  }
+};
