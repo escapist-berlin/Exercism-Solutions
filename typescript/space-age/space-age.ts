e48978d3ec9e276b0bbdc46e28aeaf402e41e2fc
Denis Kolokolov <dennis.kolokolov@gmail.com>
2024-10-23 09:45:03 +0200

    refactor: Space Age

--- typescript/space-age/space-age.ts
+++ typescript/space-age/space-age.ts
@@ -3,19 +3,16 @@ interface OrbitalPeriodMap {
 }
 
 const orbitalPeriodMap: OrbitalPeriodMap = {
-  'Mercury': 0.2408467,
-  'Venus': 0.61519726,
-  'Earth': 1.0,
-  'Mars': 1.8808158,
-  'Jupiter': 11.862615,
-  'Saturn': 29.447498,
-  'Uranus': 84.016846,
-  'Neptune': 164.79132,
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  earth: 1.0,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132,
 }
 
 export function age(planet: string, seconds: number): number {
-  const orbitalPeriod: number = orbitalPeriodMap[planet.charAt(0).toUpperCase() + planet.slice(1)];
-  const yearInSeconds: number = 31557600 * orbitalPeriod;
-
-  return Math.round((seconds / yearInSeconds) * 100) / 100;
+  return Number((seconds / 31557600 / orbitalPeriodMap[planet]).toFixed(2));
 }
