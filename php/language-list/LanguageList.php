7e978c0f09c7a32afa5144df6298cff8055b3dab
Denis Kolokolov <dennis.kolokolov@gmail.com>
2024-10-30 22:37:46 +0100

    Language List - testAddingToLanguageList

--- php/language-list/LanguageList.php
+++ php/language-list/LanguageList.php
@@ -4,3 +4,10 @@ function language_list(...$languages)
 {
     return [...$languages];
 }
+
+function add_to_language_list($languages, $new_language)
+{
+    $languages[] = $new_language;
+
+    return $languages;
+}
